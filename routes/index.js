0b0eaa42a689e092d1a67fcc87d56640c4f843de
DDRAGON <davide.0829@gmail.com>
2018-09-20 22:23:43 +0900

    今日やったところまで

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    let displayName = '';
+    let thumbUrl = '';
+    if (req.user) {
+        displayName = req.user.displayName;
+        thumbUrl = req.user.photos[0].value;
+    }
+  res.render('index', { title: '人狼オンライン', displayName: displayName, thumbUrl: thumbUrl, ipAddress: config.ipAddress });
 });
 
 module.exports = router;
