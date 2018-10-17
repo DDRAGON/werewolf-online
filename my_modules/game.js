6f1b0c331bba8bd100011e39f6eb5d7b67a4982c
DDRAGON <davide.0829@gmail.com>
2018-10-17 14:25:50 +0900

    役職一覧の制作まで

--- my_modules/game.js
+++ my_modules/game.js
@@ -88,7 +88,7 @@ function startGame(tableId) {
     gameObj.EntryRootIo.emit('tables data', tablesInfo);
 
     addAIs(tableId); // AI の追加
-    // 役職決め
+    addRoles(tableId); // 役職決め
 
 
     gameObj.tableSocketsMap.get(tableId).emit('game start', {});
@@ -103,15 +103,73 @@ function registerTablesRootIo(tableSocketsMap) {
 }
 
 function addAIs(tableId) {
-    for(let aiId = 1; aiId < (10 - gameObj.tables[tableId].players.size); aiId++) {
+    for(let aiId = 1; aiId <= (10 - gameObj.tables[tableId].players.size); aiId++) {
         const ai = {
             tableId,
             displayName: `AI${aiId}`,
-            thumbUrl: nul,
+            thumbUrl: null,
             twitterId: `AI${aiId}`
         };
         tables[tableId].AIs.set(`AI${aiId}`, ai);
     }
+}
+
+function addRoles(tableId) {
+    const numOfPlayers = gameObj.tables[tableId].players.size + gameObj.tables[tableId].AIs.size;
+    const rolesArray = createRolesArray(numOfPlayers);
+    console.log({numOfPlayers, rolesArray, players: gameObj.tables[tableId].players, AIs: gameObj.tables[tableId].AIs});
+}
+
+// 参考 https://ruru-jinro.net/cast.jsp
+function createRolesArray(numOfPlayers) {
+    switch (numOfPlayers) {
+        case 10:
+            return ['村人', '村人', '村人', '村人', '村人', '占い師', '霊能者', '狂人', '人狼', '人狼'];
+        case 11:
+            return ['村人', '村人', '村人', '村人', '村人', '占い師', '霊能者', '狩人', '狂人', '人狼', '人狼'];
+        case 12:
+            return ['村人', '村人', '村人', '村人', '村人', '村人', '占い師', '霊能者', '狩人', '狂人', '人狼', '人狼'];
+        case 13:
+            return ['村人', '村人', '村人', '村人', '村人', '共有者', '共有者', '占い師', '霊能者', '狩人', '狂人', '人狼', '人狼'];
+        case 14:
+            return ['村人', '村人', '村人', '村人', '村人', '共有者', '共有者', '占い師', '霊能者', '狩人', '妖狐', '狂人', '人狼', '人狼'];
+        case 15:
+            return ['村人', '村人', '村人', '村人', '村人', '村人', '共有者', '共有者', '占い師', '霊能者', '狩人', '妖狐', '狂人', '人狼', '人狼'];
+        case 16:
+            return ['村人', '村人', '村人', '村人', '村人', '村人', '共有者', '共有者', '占い師', '霊能者', '狩人', '妖狐', '狂人', '人狼', '人狼', '人狼'];
+    }
+
+    // これ以上プレイヤー数が多い場合は一般化して求める。
+    const numOfWerewolfs = Math.floor(numOfPlayers / 5); // 人狼の数
+    const numOfMadmans  = Math.floor(numOfPlayers / 28) + 1; // 狂人の数
+    const numOfShares = Math.floor(numOfPlayers / 13) + 1; // 共有者の数;
+    const numOfInus = Math.floor(numOfPlayers / 14); // 妖狐の数;
+    const numOfHunters = Math.floor(numOfPlayers / 30) + 1; // 狩人の数;
+    const numOfVillager = numOfPlayers - numOfWerewolfs - numOfMadmans - numOfShares - numOfInus - numOfHunters - 2; // 占い師と霊能者の数をひく
+
+    const rolesArray = [];
+    for (let i = 0; i < numOfVillager; i++) {
+        rolesArray.push('村人');
+    }
+    for (let i = 0; i < numOfShares; i++) {
+        rolesArray.push('共有者');
+    }
+    rolesArray.push('占い師');
+    rolesArray.push('霊能者');
+    for (let i = 0; i < numOfHunters; i++) {
+        rolesArray.push('狩人');
+    }
+    for (let i = 0; i < numOfInus; i++) {
+        rolesArray.push('妖狐');
+    }
+    for (let i = 0; i < numOfMadmans; i++) {
+        rolesArray.push('狂人');
+    }
+    for (let i = 0; i < numOfWerewolfs; i++) {
+        rolesArray.push('人狼');
+    }
+
+    return rolesArray;
 }
 
 function calcPlayerId(tableId, displayName, twitterId) {
